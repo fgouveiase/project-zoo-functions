64fd43b89240fb86d69389994a47431d44f0a1c5
felipe <felipegouveia.bfr@hotmail.com>
2022-10-03 15:18:11 -0300

    resolvendo questao 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((person) => person.age < 18).length;
+  const adult = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const senior = entrants.filter((person) => person.age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui.
+  if (entrants === undefined || entrants.length === undefined) {
+    return 0;
+  }
+  const visitantesSum = countEntrants(entrants);
+  return (visitantesSum.child * prices.child)
+  + (visitantesSum.adult * prices.adult) + (visitantesSum.senior * prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
