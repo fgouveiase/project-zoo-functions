34a8c1c0fb9d3b98e6b041cb88fda0876e6a0b5b
felipe <felipegouveia.bfr@hotmail.com>
2022-10-03 15:39:26 -0300

    resolvendo questao 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+const objeto = employees.map((elemento) => {
+  const obj = {
+    id: elemento.id,
+    fullName: `${elemento.firstName} ${elemento.lastName}`,
+    species: elemento.responsibleFor.map((param) => species
+      .find((animal) => animal.id === param).name),
+    locations: elemento.responsibleFor.map((param) => species
+      .find((animal) => animal.id === param).location),
+  };
+  return obj;
+});
+function getEmployeesCoverage(func) {
+  if (func === undefined) {
+    return objeto;
+  }
+  const dados = objeto;
+  const tne = dados.find((e) => e.fullName
+    .includes(Object.values(func)) || e.id.includes(Object.values(func)));
+  if (tne === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return tne;
 }
 
 module.exports = getEmployeesCoverage;
